a0231c156b5e4aedf8c7cac94eff8adfd71b775d
Damiano Ravalico <damianoravalico.uni@gmail.com>
2022-11-02 16:42:42 +0100

    Return string 1

---
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzzConverter.java
@@ -0,0 +1,13 @@
+package dssc.assignment.fizzbuzz;
+
+public class FizzBuzzConverter {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String fizzBuzzConvert(int number) {
+        return "1";
+    }
+
+}
--- src/test/java/dssc/assignment/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/dssc/assignment/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,14 @@
 package dssc.assignment.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void convertIntegerToString() {
+        FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
+        assertEquals("1", fizzBuzzConverter.fizzBuzzConvert(1));
+    }
 }
