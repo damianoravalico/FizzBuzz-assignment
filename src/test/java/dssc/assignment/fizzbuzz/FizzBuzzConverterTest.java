b1119b1ed8835a6bafca56f757b5d418a6716e11
Damiano Ravalico <damianoravalico.uni@gmail.com>
2022-11-07 17:00:24 +0100

    FizzBuzzConverter is now a utility class

--- src/test/java/dssc/assignment/fizzbuzz/FizzBuzzConverterTest.java
+++ src/test/java/dssc/assignment/fizzbuzz/FizzBuzzConverterTest.java
@@ -11,53 +11,45 @@ public class FizzBuzzConverterTest {
 
     @Test
     public void convertOneAsIntegerToString() {
-        FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-        assertEquals("1", fizzBuzzConverter.convert(1));
+        assertEquals("1", FizzBuzzConverter.convert(1));
     }
 
     @Test
     public void convertTwoAsIntegerToString() {
-        FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-        assertEquals("2", fizzBuzzConverter.convert(2));
+        assertEquals("2", FizzBuzzConverter.convert(2));
     }
 
     @Test
     public void convertThreeAsIntegerToFizz() {
-        FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-        assertEquals("Fizz", fizzBuzzConverter.convert(3));
+        assertEquals("Fizz", FizzBuzzConverter.convert(3));
     }
 
     @Test
     public void convertThreeMultiplesToFizz() {
-        FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-        assertEquals("Fizz", fizzBuzzConverter.convert(6));
+        assertEquals("Fizz", FizzBuzzConverter.convert(6));
     }
 
     @Test
     public void convertFiveAsIntegerToBuzz() {
-        FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-        assertEquals("Buzz", fizzBuzzConverter.convert(5));
+        assertEquals("Buzz", FizzBuzzConverter.convert(5));
     }
 
     @Test
     public void convertFiveMultiplesToFizz() {
-        FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-        assertEquals("Buzz", fizzBuzzConverter.convert(10));
+        assertEquals("Buzz", FizzBuzzConverter.convert(10));
     }
 
     @Test
     public void convertThreeAndFiveMultiplesToFizzBuzz() {
-        FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-        assertEquals("FizzBuzz", fizzBuzzConverter.convert(15));
-        assertEquals("FizzBuzz", fizzBuzzConverter.convert(30));
+        assertEquals("FizzBuzz", FizzBuzzConverter.convert(15));
+        assertEquals("FizzBuzz", FizzBuzzConverter.convert(30));
     }
 
     @Test
     public void convertSomeNumbers() {
-        FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-        assertEquals("FizzBuzz", fizzBuzzConverter.convert(45));
-        assertEquals("Fizz", fizzBuzzConverter.convert(9));
-        assertEquals("Buzz", fizzBuzzConverter.convert(25));
+        assertEquals("FizzBuzz", FizzBuzzConverter.convert(45));
+        assertEquals("Fizz", FizzBuzzConverter.convert(9));
+        assertEquals("Buzz", FizzBuzzConverter.convert(25));
     }
 
     @Test
